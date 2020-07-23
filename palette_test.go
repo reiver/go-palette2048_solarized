f72bef9c5c40abe758475b275f23280d3595e9de
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-22 19:37:56 -0700

    initial commits

---
+++ palette_test.go
@@ -0,0 +1,36 @@
+package palette2048_solarized_test
+
+import (
+	"github.com/reiver/go-palette2048"
+	"github.com/reiver/go-palette2048_solarized"
+
+	"fmt"
+	"image/color"
+
+	"testing"
+)
+
+func TestPalette_palette2048(t *testing.T) {
+
+	var palette palette2048.Slice = palette2048.Slice(palette2048_solarized.Palette[:])
+
+	if nil == palette {
+		t.Error("This should never happen.")
+		return
+	}
+
+	{
+		var yellow color.Color = color.RGBA{255,199,6,55}
+
+		actualColor := palette.Convert(yellow)
+		actual := fmt.Sprint(actualColor)
+
+		expected := "rgba(248,184,0,255)---" // #F8B800
+
+		if expected != actual {
+			t.Errorf("The actual color was not what was expected.")
+			t.Log("EXPECTED:", expected)
+			t.Log("ACTUAL:  ", actual)
+		}
+	}
+}
